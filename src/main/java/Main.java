c41d11c19836031dbabd206dc1ddaa663e839122
SSS5900 <sosnovskihsergey@gmail.com>
2023-03-14 21:09:45 +0500

    Rename percent to Percent

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ import java.sql.SQLOutput;
 
 public class Main {
 
-    public static void main(String[] args){
+    public static void main(String[] args) {
         BonusService service = new BonusService();
         long bonus = service.calculate(5_000, true);
         System.out.println(bonus);
